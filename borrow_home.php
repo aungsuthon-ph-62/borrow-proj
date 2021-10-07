bd988bf1433f3dcae48e218b475dddc355a3af5f
● YOUNGBOY MONEZXD ● <85674453+aungsuthon-ph-62@users.noreply.github.com>
2021-10-08 04:23:23 +0700

    Initial commit

---
+++ borrow_home.php
@@ -0,0 +1,71 @@
+<?php include "php/read_borrow_home"; ?>
+<?php include "resource/env/header.php"; ?>
+
+
+
+<body>
+<?php include "resource/env/navbar.php"; ?>
+
+	<div class="container">
+		<div class="my-4">
+			<h4 class="display-4 text-left">ยืม&คืน</h4>
+		</div>
+
+		<div class="link-right my-4">
+			<a href="create.php" class="btn btn-info text-white"><i class="fas fa-plus"></i> Create</a>
+		</div>
+		<?php if (mysqli_num_rows($result)) { ?>
+			<div class="table-responsive">
+				<table class="table  table-striped table-hover border border-dark border-3 rounded shadow">
+					<thead class="table-dark">
+						<tr>
+							<th scope="col" class="text-center">ลำดับ</th>
+							<th scope="col" class="text-center">รหัสอุปกรณ์</th>
+							<th scope="col" class="text-center">รหัสผู้ยืม</th>
+							<th scope="col" class="text-center">วันเดือนปีที่ยืม</th>
+							<th scope="col" class="text-center">ผู้อนุมัติ</th>
+							<th scope="col" class="text-center">กำหนดส่ง</th>
+							<th scope="col" class="text-center">สถานะ</th>
+							<th scope="col" class="text-center">จัดการ</th>
+						</tr>
+					</thead>
+					<tbody class="table-light">
+						<?php
+						$i = 0;
+						while ($rows = mysqli_fetch_assoc($result)) {
+							$i++;
+						?>
+							<tr>
+								<th scope="row"><?= $i ?></th>
+								<td class="text-center"><?= $rows['device_no'] ?></td>
+								<td class="text-center"><?php echo $rows['student_id']; ?></td>
+								<td class="text-center"><?php echo $rows['borrow_date']; ?></td>
+								<td class="text-center"><?php echo $rows['t_name']; ?></td>
+								<td class="text-center"><?php echo $rows['return_date']; ?></td>
+								<td class="text-center">
+									<?php
+									$r = $rows['borrow_status'];
+									if ($r == 0) {
+										echo "ว่าง";
+									} else {
+										echo "ไม่ว่าง";
+									}
+									?>
+								</td>
+								<td class="text-center">
+									<div class="d-grid gap-2 px-3">
+										<a href="update.php?id=<?= $rows['b_id'] ?>" class="btn btn-success btn-sm"> <i class="fas fa-edit"></i> Update</a>
+										<a href="php/delete.php?id=<?= $rows['b_id'] ?>" class="btn btn-danger btn-sm"> <i class="fas fa-minus-circle"></i> Delete</a>
+									</div>
+								</td>
+							</tr>
+						<?php } ?>
+					</tbody>
+				</table>
+			</div>
+		<?php } ?>
+	</div>
+
+</body>
+
+</html>
---
+++ resource/env/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Borrow-Project</title>
+	<link rel="stylesheet" href="/resource/css/main.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+	<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+
+<style>
+	body {
+		font-family: 'Prompt', sans-serif;
+	}
+</style>
